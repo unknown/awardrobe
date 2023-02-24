30cbb30eeb089ba95bf65b450d666746f735552c
David Mo <44306450+unknown@users.noreply.github.com>
2023-02-24 15:21:49 -0500

    Add multiprocessing to Uniqlo script

---
+++ scripts/uniqlo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from multiprocessing import Pool
+import requests
+import json
+
+endpoint = "http://localhost:3001/uniqlo-us/"
+headers = {
+    "Content-Type": "application/json"
+}
+productIds = [
+    "E457264-000",  # U Knitted Short-Sleeve Polo Shirt
+    "E457967-000",  # U Wide-Fit Pleated Chino Pants
+    "E453056-000",  # Easy Care Stretch Slim-Fit Long-Sleeve Shirt
+]
+
+
+def get_product_data(product_id):
+    payload = json.dumps({
+        "productId": product_id
+    })
+    response = requests.request("GET", endpoint,
+                                headers=headers, data=payload)
+    print(f'{response.status_code}: {len(response.json())} entries')
+
+
+def main():
+    with Pool(processes=4) as p:
+        p.map(get_product_data, productIds)
+
+
+if __name__ == "__main__":
+    main()
