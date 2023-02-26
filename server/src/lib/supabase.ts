d09fc2765b7f4aa9f184a7db7b424fca618aaf17
David Mo <44306450+unknown@users.noreply.github.com>
2023-02-26 16:58:11 -0500

    Remove path alias in server

--- server/src/lib/supabase.ts
+++ server/src/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "@/lib/database.types";
+import { Database } from "./database.types";
 
 export const supabase = createClient<Database>(
   process.env.SUPABASE_URL!,
