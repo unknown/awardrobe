147c948abb4f482a2522e97a0504c2a97663e69b
David Mo <44306450+unknown@users.noreply.github.com>
2023-03-04 20:30:42 -0500

    Handle invalid product ID

--- server/src/services/uniqlo.ts
+++ server/src/services/uniqlo.ts
@@ -17,30 +17,36 @@ function getPriceEndpoint(productId: string) {
 
 // TODO: cache product ids to save on DB reads and error handling
 async function getProductDbId(productId: string) {
-  const { data } = await supabase
+  const { data, error } = await supabase
     .from("products")
     .select()
-    .eq("product_id", productId);
+    .eq("product_id", productId)
+    .maybeSingle();
 
-  return data![0].id;
+  if (error) {
+    console.error(error);
+  }
+
+  return data?.id;
 }
 
 export async function getItemData(productId: string) {
   const priceEndpoint = getPriceEndpoint(productId);
+  const itemData: ItemData[] = [];
+
+  const productDbId = await getProductDbId(productId);
+  if (!productDbId) return itemData;
 
   const {
     result: { stocks, prices, l2s },
   } = (await axios.get(priceEndpoint)).data;
 
-  const itemData: ItemData[] = [];
   await Promise.all(
     Object.keys(stocks).map(async (key, index) => {
       const style: string = l2s[index].color.displayCode.toString();
       const size: string = l2s[index].size.displayCode.toString();
       const stock: number = parseInt(stocks[key].quantity);
       const price: string = prices[key].base.value.toString();
-
-      const productDbId = await getProductDbId(productId);
 
       itemData.push({
         product_id: productDbId,
