c4cefb2af7cf472cf85c408871faf56400f80d7d
David Mo <44306450+unknown@users.noreply.github.com>
2023-06-07 18:47:04 -0400

    Add custom icons

---
+++ web/src/components/icons/Check.tsx
@@ -0,0 +1,24 @@
+import { SVGProps, forwardRef } from "react";
+
+export const Check = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>((props, ref) => {
+  return (
+    <svg
+      width="1.5rem"
+      height="1.5rem"
+      strokeWidth={1.5}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      ref={ref}
+      {...props}
+    >
+      <path
+        d="M5 12L10 17L20 7"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+});
+Check.displayName = "Check";
---
+++ web/src/components/icons/ChevronDown.tsx
@@ -0,0 +1,24 @@
+import { SVGProps, forwardRef } from "react";
+
+export const ChevronDown = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>((props, ref) => {
+  return (
+    <svg
+      width="1.5rem"
+      height="1.5rem"
+      strokeWidth={1.5}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      ref={ref}
+      {...props}
+    >
+      <path
+        d="M6 9l6 6 6-6"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+    </svg>
+  );
+});
+ChevronDown.displayName = "ChevronDown";
