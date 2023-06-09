bc4dac2b7cac5640db9913bce2fc9e7fa10c29f5
David Mo <44306450+unknown@users.noreply.github.com>
2023-06-09 17:16:13 -0400

    Add Prisma seed script

---
+++ packages/database/prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const uniqlo = await prisma.store.upsert({
+    where: { handle: "uniqlo-us" },
+    update: {},
+    create: {
+      name: "Uniqlo US",
+      handle: "uniqlo-us",
+      externalUrl: "https://www.uniqlo.com/",
+    },
+  });
+
+  console.log({ uniqlo });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
