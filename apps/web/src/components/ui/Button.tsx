3fa9c5d2ab99b9be4a26df9bda672bb0cc2f05be
David Mo <44306450+unknown@users.noreply.github.com>
2023-06-12 16:25:09 -0400

    Update button style

--- apps/web/src/components/ui/Button.tsx
+++ apps/web/src/components/ui/Button.tsx
@@ -2,24 +2,27 @@ import { cn } from "@/utils/utils";
 import { VariantProps, cva } from "class-variance-authority";
 import { ButtonHTMLAttributes, forwardRef } from "react";
 
-const buttonVariants = cva("inline-flex items-center justify-center rounded-md transition-colors", {
-  variants: {
-    variant: {
-      primary: "bg-primary text-primary-foreground hover:bg-primary/90",
-      secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-      outline: "border border-input hover:bg-accent hover:text-accent-foreground",
+const buttonVariants = cva(
+  "inline-flex items-center justify-center rounded-md transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
+  {
+    variants: {
+      variant: {
+        primary: "bg-primary text-primary-foreground hover:bg-primary/90",
+        secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        outline: "border border-input hover:bg-accent hover:text-accent-foreground",
+      },
+      size: {
+        sm: "px-2 py-1",
+        default: "px-4 py-2",
+        lg: "px-8 py-3",
+      },
     },
-    size: {
-      sm: "px-2 py-1",
-      default: "px-4 py-2",
-      lg: "px-8 py-3",
+    defaultVariants: {
+      variant: "primary",
+      size: "default",
     },
-  },
-  defaultVariants: {
-    variant: "primary",
-    size: "default",
-  },
-});
+  }
+);
 
 export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonVariants>;
