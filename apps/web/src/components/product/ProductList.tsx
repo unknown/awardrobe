f939b99f971d04e1681172228d03e156ec0970c6
David Mo <44306450+unknown@users.noreply.github.com>
2023-07-19 17:01:35 -0400

    Make the entire product card a link

--- apps/web/src/components/product/ProductList.tsx
+++ apps/web/src/components/product/ProductList.tsx
@@ -16,10 +16,12 @@ export function ProductList({ products }: ProductListProps) {
       <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3">
         {products.map((product) => {
           return (
-            <div key={product.id} className="rounded-md border p-3">
-              <p className="text-muted-foreground text-sm">{product.storeName}</p>
-              <Link href={`/product/${product.id}`}>{product.name}</Link>
-            </div>
+            <Link href={`/product/${product.id}`}>
+              <div key={product.id} className="h-full rounded-md border p-3">
+                <p className="text-muted-foreground text-sm">{product.storeName}</p>
+                <p>{product.name}</p>
+              </div>
+            </Link>
           );
         })}
       </div>
