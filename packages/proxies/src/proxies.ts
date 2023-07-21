43772425fd15fb75664d263dd28849e967c11aa5
David Mo <44306450+unknown@users.noreply.github.com>
2023-07-21 19:20:23 -0400

    Remove proxies that don't work on Abercrombie

--- packages/proxies/src/proxies.ts
+++ packages/proxies/src/proxies.ts
@@ -4,9 +4,9 @@ export const proxies = [
   "http://cap116:wrFOZ@154.3.57.115:61234",
   "http://cap1940:ugySW@154.22.4.147:61234",
   "http://cap2492:VUrFz@154.21.235.187:61234",
-  "http://cap79:YuMcR@50.118.247.142:61234",
   "http://isp1037:oSUmb@5.182.123.12:61234",
-  "http://cap87:rfknc@50.118.247.150:61234",
+  // "http://cap79:YuMcR@50.118.247.142:61234",
+  // "http://cap87:rfknc@50.118.247.150:61234",
 ];
 
 const agentFromProxy: Map<string, HttpsProxyAgent<string>> = new Map();
