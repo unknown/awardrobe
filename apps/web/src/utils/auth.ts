9afd0c31cfb743261db1927d60dd17fa0b6bc042
David Mo <44306450+unknown@users.noreply.github.com>
2023-07-24 11:11:54 -0400

    Update auth options

--- apps/web/src/utils/auth.ts
+++ apps/web/src/utils/auth.ts
@@ -1,17 +1,23 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { render } from "@react-email/render";
-import { NextAuthOptions } from "next-auth";
+import { NextAuthOptions, type DefaultSession } from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import { createTransport } from "nodemailer";
 
 import { SignInEmail } from "@awardrobe/emails";
 import { prisma } from "@awardrobe/prisma-types";
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
-  pages: {
-    signIn: "/login",
-  },
+  pages: { signIn: "/login" },
   providers: [
     EmailProvider({
       server: process.env.EMAIL_SERVER,
@@ -19,24 +25,24 @@ export const authOptions: NextAuthOptions = {
       sendVerificationRequest: async ({ identifier, url, provider }) => {
         const transport = createTransport(provider.server);
         const emailHtml = render(SignInEmail({ url }));
-        const result = await transport.sendMail({
+        await transport.sendMail({
           to: identifier,
           from: provider.from,
           subject: "Sign in to Awardrobe",
           html: emailHtml,
         });
-
-        const failed = result.rejected.concat(result.pending).filter(Boolean);
-        if (failed.length) {
-          throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`);
-        }
       },
     }),
   ],
   callbacks: {
-    async session({ session, user }) {
-      session.user.id = user.id;
-      return Promise.resolve(session);
+    session({ session, user }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+        },
+      };
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
