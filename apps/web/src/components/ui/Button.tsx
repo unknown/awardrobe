19b8c29bf553592952858ae56cb946e5dd9abce1
David Mo <44306450+unknown@users.noreply.github.com>
2023-07-29 16:00:10 -0400

    Remove destructive outline button variant

--- apps/web/src/components/ui/Button.tsx
+++ apps/web/src/components/ui/Button.tsx
@@ -4,16 +4,14 @@ import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "@/utils/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
+  "focus-visible:ring-ring ring-offset-background inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         primary: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        outline: "border border-input hover:bg-accent hover:text-accent-foreground",
-        destructiveOutline:
-          "border border-destructive text-destructive hover:bg-destructive hover:text-destructive-foreground",
+        outline: "bg-background border-input hover:bg-accent hover:text-accent-foreground border",
+        destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
       },
       size: {
         default: "h-10 px-4 py-2",
