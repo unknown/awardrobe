0fb1642c1cbbb140f25fc2e82313bff0c00753ce
David Mo <44306450+unknown@users.noreply.github.com>
2023-08-07 13:40:34 -0400

    Add J Crew adapter

---
+++ packages/adapters/src/adapters/jcrew-us/index.ts
@@ -0,0 +1,80 @@
+import axios from "axios";
+
+import { getRandomHttpsProxyAgent } from "@awardrobe/proxies";
+
+import { dollarsToCents, toTitleCase } from "../../utils/formatter";
+import { StoreAdapter, VariantInfo } from "../../utils/types";
+import { productInfoSchema } from "./schemas";
+
+const JCrewUS: StoreAdapter = {
+  urlPrefixes: ["jcrew.com"],
+  storeHandle: "jcrew-us",
+  getProducts,
+  getProductCode,
+  getProductDetails,
+};
+export default JCrewUS;
+
+// TODO: implement
+async function getProducts(_?: number) {
+  return [];
+}
+
+async function getProductCode(url: string) {
+  const productCodeRegex = /\/([a-zA-Z0-9]+)/g;
+  const matches = url.match(productCodeRegex);
+
+  const productCode = matches?.at(-1)?.slice(1);
+  if (!productCode) {
+    throw new Error(`Failed to get product code from ${url}`);
+  }
+
+  return productCode;
+}
+
+async function getProductDetails(productCode: string) {
+  const productEndpoint = `https://app.jcrew.com/browse/products/${productCode}`;
+  const httpsAgent = getRandomHttpsProxyAgent();
+  const headers = {
+    "User-Agent": "JCrew/1.0.8 (com.jcrew.jcrew; build:686; iOS 16.5.1) Alamofire/5.7.1",
+  };
+  const params = {
+    "country-code": "US",
+    display: "standard",
+    expand: "availability,prices,variations,set_products",
+    locale: "en-US",
+  };
+  const productResponse = await axios.get(productEndpoint, { httpsAgent, headers, params });
+  const timestamp = new Date();
+
+  const productInfo = productInfoSchema.parse(productResponse.data);
+  const products = "set_products" in productInfo ? productInfo.set_products : [productInfo];
+
+  const variants: VariantInfo[] = products.flatMap((product) => {
+    return product.variants.map((variant) => {
+      const attributes = Object.entries(variant.variation_values).map(([key, value]) => {
+        const attributeOptions = product.variation_attributes.find((attr) => attr.id === key);
+        const attributeName = attributeOptions?.name;
+        const attributeValue = attributeOptions?.values.find(
+          (val) => val.value === variant.variation_values[key],
+        )?.name;
+        return {
+          name: attributeName ?? key,
+          value: toTitleCase(attributeValue ?? value),
+        };
+      });
+      return {
+        timestamp,
+        attributes,
+        productUrl: "",
+        inStock: variant.orderable,
+        priceInCents: dollarsToCents(variant.price.toString()),
+      };
+    });
+  });
+
+  return {
+    variants,
+    name: productInfo.name,
+  };
+}
---
+++ packages/adapters/src/adapters/jcrew-us/schemas.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+export const productSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  variants: z.array(
+    z.object({
+      orderable: z.boolean(),
+      price: z.number(),
+      product_id: z.string(),
+      variation_values: z.record(z.string()),
+    }),
+  ),
+  variation_attributes: z.array(
+    z.object({
+      id: z.string(),
+      name: z.string(),
+      values: z.array(
+        z.object({
+          name: z.string(),
+          value: z.string(),
+        }),
+      ),
+    }),
+  ),
+});
+export const productInfoSchema = z.union([
+  z.object({
+    name: z.string(),
+    set_products: z.array(productSchema),
+  }),
+  productSchema,
+]);
