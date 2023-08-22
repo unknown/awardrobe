d2506d9d9e010a52361e4fd96a7a9b0cf67ca34f
David Mo <44306450+unknown@users.noreply.github.com>
2023-08-22 13:32:32 -0400

    Add bell icon for products with notifications

--- apps/web/src/components/product/ProductList.tsx
+++ apps/web/src/components/product/ProductList.tsx
@@ -1,22 +1,46 @@
 import Link from "next/link";
+import { Bell } from "@icons/Bell";
+import { getServerSession } from "next-auth";
 
 import { Product } from "@awardrobe/meilisearch-types";
+import { prisma } from "@awardrobe/prisma-types";
+
+import { authOptions } from "@/utils/auth";
 
 export type ProductListProps = {
   products: Product[];
 };
 
-export function ProductList({ products }: ProductListProps) {
+export async function ProductList({ products }: ProductListProps) {
   if (products.length === 0) {
     return <p className="text-center">No matching products found.</p>;
   }
 
+  const session = await getServerSession(authOptions);
+  const notifications = session
+    ? await prisma.productNotification.findMany({
+        where: {
+          productVariant: { productId: { in: products.map((product) => product.id) } },
+          userId: session.user.id,
+        },
+        include: { productVariant: true },
+      })
+    : [];
+
   return (
     <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3">
       {products.map((product) => {
+        const hasNotification = notifications.some(
+          (notification) => notification.productVariant.productId === product.id,
+        );
         return (
           <Link key={product.id} href={`/product/${product.id}`}>
-            <div className="h-full rounded-md border p-4 md:p-3">
+            <div className="relative h-full rounded-md border p-4 md:p-3">
+              {hasNotification ? (
+                <div className="absolute right-4 top-4 md:right-3 md:top-3">
+                  <Bell className="h-4 w-4" strokeWidth={2} />
+                </div>
+              ) : null}
               <p className="text-muted-foreground text-sm">{product.storeName}</p>
               <p>{product.name}</p>
             </div>
