af01c2f7241bbc631dbd100325b2666e588f52f4
David Mo <44306450+unknown@users.noreply.github.com>
2023-10-09 10:14:20 -0400

    Fix browse loading page to match product list loading skeleton

--- apps/web/src/app/(app)/(browse)/loading.tsx
+++ apps/web/src/app/(app)/(browse)/loading.tsx
@@ -3,13 +3,9 @@ import { Skeleton } from "@ui/Skeleton";
 export default function Loading() {
   return (
     <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3">
-      <Skeleton className="h-[48px]" />
-      <Skeleton className="h-[48px]" />
-      <Skeleton className="h-[48px]" />
-      <Skeleton className="h-[48px]" />
-      <Skeleton className="h-[48px]" />
-      <Skeleton className="h-[48px]" />
-      <Skeleton className="h-[48px]" />
+      {[...Array(6)].map((_, i) => (
+        <Skeleton key={i} className="h-[48px]" />
+      ))}
     </div>
   );
 }
