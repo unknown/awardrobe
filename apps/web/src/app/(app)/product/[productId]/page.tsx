9e28e46b77dbf807fdf5efacadc9d4284b19b1b1
David Mo <44306450+unknown@users.noreply.github.com>
2023-12-12 01:48:32 -0500

    Add price suspense boundaries

--- apps/web/src/app/(app)/product/[productId]/page.tsx
+++ apps/web/src/app/(app)/product/[productId]/page.tsx
@@ -1,8 +1,12 @@
+import { Fragment, Suspense } from "react";
 import { notFound } from "next/navigation";
+import { Bell } from "@icons/Bell";
+import { Button } from "@ui/Button";
 
 import { VariantAttribute } from "@awardrobe/adapters";
-import { findPrices, findProductWithVariants } from "@awardrobe/db";
+import { findPrices, findProductWithVariants, ProductWithVariants } from "@awardrobe/db";
 import { getProductPath } from "@awardrobe/media-store";
+import { Price, ProductVariant } from "@awardrobe/prisma-types";
 
 import { NotificationPopover } from "@/components/notification/NotificationPopover";
 import { ChartPrice, ProductChart } from "@/components/product/ProductChart";
@@ -65,25 +69,17 @@ export default async function ProductPage({
     });
   }
 
-  const mediaStorePath = getProductPath(product.id);
-  const mediaUrl = new URL(mediaStorePath, process.env.NEXT_PUBLIC_MEDIA_STORE_URL).href;
-
-  const prices = variant
-    ? await findPrices({
+  const pricesPromise = variant
+    ? findPrices({
         variantId: variant.id,
         startDate: getDateFromRange(initialDateRange).toISOString(),
       })
     : null;
 
-  const lastPrice = prices?.at(-1);
-  const chartPrices: ChartPrice[] | null =
-    prices !== null && lastPrice
-      ? [...prices, { ...lastPrice, timestamp: new Date().toISOString() }].map((price) => ({
-          date: price.timestamp.toString(),
-          price: price.priceInCents,
-          stock: price.inStock ? 1 : 0,
-        }))
-      : null;
+  const mediaStorePath = getProductPath(product.id);
+  const mediaUrl = new URL(mediaStorePath, process.env.NEXT_PUBLIC_MEDIA_STORE_URL).href;
+
+  const suspenseKey = `${variant?.id}-${initialDateRange}`;
 
   return (
     <section className="space-y-12">
@@ -92,7 +88,7 @@ export default async function ProductPage({
           <div className="h-64 min-w-[16rem]">
             <img className="h-full w-full object-contain" src={mediaUrl} />
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex w-full flex-col gap-2">
             <p className="text-muted-foreground text-sm">{product.store.name}</p>
             <h1 className="text-3xl font-medium">{product.name}</h1>
             <VariantControls
@@ -100,27 +96,27 @@ export default async function ProductPage({
               productOptions={productOptions}
             />
             <div className="flex flex-wrap gap-3">
-              <a
-                className="text-md inline-block rounded-md bg-sky-500 px-4 py-2 font-medium text-white hover:bg-sky-600"
-                href={variant?.productUrl}
-                target="_blank"
-                rel="noopener noreferrer"
+              <Suspense
+                key={suspenseKey}
+                fallback={
+                  <Fragment>
+                    <p className="text-md rounded-md bg-sky-500 px-4 py-2 font-medium text-white">
+                      Loading...
+                    </p>
+                    <Button variant="secondary" size="icon" disabled>
+                      <Bell />
+                    </Button>
+                  </Fragment>
+                }
               >
-                {!variant?.productUrl
-                  ? "Not available"
-                  : lastPrice === undefined
-                    ? "See price"
-                    : `${formatCurrency(lastPrice.priceInCents)} on ${
-                        product.store.shortenedName ?? product.store.name
-                      }`}
-              </a>
-              <NotificationPopover
-                productId={product.id}
-                productOptions={productOptions}
-                variants={product.variants}
-                attributes={initialAttributes}
-                priceInCents={lastPrice?.priceInCents ?? null}
-              />
+                <PriceControls
+                  pricesPromise={pricesPromise}
+                  product={product}
+                  variant={variant ?? null}
+                  productOptions={productOptions}
+                  initialAttributes={initialAttributes}
+                />
+              </Suspense>
             </div>
           </div>
         </div>
@@ -138,16 +134,98 @@ export default async function ProductPage({
             </span>
           </div>
         </div>
-        {prices?.length === 1000 ? (
-          <div className="rounded-md border border-yellow-300 bg-yellow-100 p-4 text-yellow-900">
-            Limited to showing only the first 1000 data points. Try applying filters to decrease the
-            number of data points.
-          </div>
-        ) : null}
         <div className="relative h-[20rem] sm:h-[24rem] md:h-[28rem]">
-          <ProductChart prices={chartPrices} />
+          <Suspense
+            key={suspenseKey}
+            fallback={
+              <Fragment>
+                <ProductChart prices={null} />
+                <p className="text-muted-foreground absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                  Loading...
+                </p>
+              </Fragment>
+            }
+          >
+            <Chart pricesPromise={pricesPromise} />
+          </Suspense>
         </div>
       </div>
     </section>
+  );
+}
+
+type PriceControlsProps = {
+  pricesPromise: Promise<Price[]> | null;
+  product: ProductWithVariants;
+  variant: ProductVariant | null;
+  productOptions: Record<string, string[]>;
+  initialAttributes: Record<string, string>;
+};
+
+async function PriceControls({
+  pricesPromise,
+  product,
+  variant,
+  productOptions,
+  initialAttributes,
+}: PriceControlsProps) {
+  const prices = await pricesPromise;
+  const lastPrice = prices?.at(-1);
+
+  const storeName = product.store.shortenedName ?? product.store.name;
+
+  return (
+    <Fragment>
+      <a
+        className="text-md inline-block rounded-md bg-sky-500 px-4 py-2 font-medium text-white hover:bg-sky-600"
+        href={variant?.productUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {!variant?.productUrl
+          ? "Not available"
+          : !lastPrice
+            ? "See price"
+            : `${formatCurrency(lastPrice.priceInCents)} at ${storeName}`}
+      </a>
+      <NotificationPopover
+        productId={product.id}
+        productOptions={productOptions}
+        variants={product.variants}
+        attributes={initialAttributes}
+        priceInCents={lastPrice?.priceInCents ?? null}
+      />
+    </Fragment>
+  );
+}
+
+type ChartProps = {
+  pricesPromise: Promise<Price[]> | null;
+};
+
+async function Chart({ pricesPromise }: ChartProps) {
+  const prices = await pricesPromise;
+  const lastPrice = prices?.at(-1);
+  const chartPrices: ChartPrice[] | null =
+    prices !== null && lastPrice
+      ? [...prices, { ...lastPrice, timestamp: new Date().toISOString() }].map((price) => ({
+          date: price.timestamp.toString(),
+          price: price.priceInCents,
+          stock: price.inStock ? 1 : 0,
+        }))
+      : null;
+
+  return (
+    <Fragment>
+      <ProductChart prices={chartPrices} />
+      {chartPrices === null && (
+        <div className="bg-gradient-radial from-background absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 to-transparent p-16 text-center">
+          <h2 className="text-xl font-medium">No price history</h2>
+          <p className="text-muted-foreground">
+            We have no data in this date range. Maybe try again later?
+          </p>
+        </div>
+      )}
+    </Fragment>
   );
 }
