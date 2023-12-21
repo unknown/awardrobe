470728232d69542061e385d0fd839a65c551ed86
David Mo <44306450+unknown@users.noreply.github.com>
2023-12-21 13:51:17 -0500

    Remove build step from internal packages

---
+++ apps/monitors/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  tsconfig: "tsconfig.json",
+  noExternal: [
+    "@awardrobe/adapters",
+    "@awardrobe/db",
+    "@awardrobe/prisma-types",
+    "@awardrobe/proxies",
+    "@awardrobe/tsconfig",
+  ],
+});
