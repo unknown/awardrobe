f0d610d7be05d5abcdf10a412e023a2347cb5e29
David Mo <44306450+unknown@users.noreply.github.com>
2023-12-22 12:36:49 -0500

    Skip node_modules in `monitors` bundling step

--- apps/monitors/tsup.config.ts
+++ apps/monitors/tsup.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
+  format: "cjs",
   tsconfig: "tsconfig.json",
+  skipNodeModulesBundle: true,
   noExternal: [
     "@awardrobe/adapters",
     "@awardrobe/db",
