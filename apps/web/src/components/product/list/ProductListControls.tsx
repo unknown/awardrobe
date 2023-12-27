c78b444d537ee73492627eaeb2d523e1e5e31c94
David Mo <44306450+unknown@users.noreply.github.com>
2023-12-27 17:37:46 -0500

    Add search icon

--- apps/web/src/components/product/list/ProductListControls.tsx
+++ apps/web/src/components/product/list/ProductListControls.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
+import { Search } from "@icons/Search";
 import { Input } from "@ui/Input";
 import debounce from "lodash.debounce";
 
@@ -26,18 +27,21 @@ export function ProductSearchbar({ searchQuery, useDebounce = false }: ProductSe
   }, [debouncedSearch]);
 
   return (
-    <Input
-      type="search"
-      className="flex-1"
-      placeholder="Search"
-      defaultValue={searchQuery}
-      onChange={(event) => useDebounce && debouncedSearch(event.target.value)}
-      onKeyDown={(event) => {
-        if (event.key === "Enter") {
-          debouncedSearch.cancel();
-          router.push(`/search?q=${event.currentTarget.value}`);
-        }
-      }}
-    />
+    <div className="relative">
+      <Search className="text-muted-foreground absolute left-2.5 top-1/2 h-4 w-4 -translate-y-1/2" />
+      <Input
+        type="search"
+        className="pl-8"
+        placeholder="Search"
+        defaultValue={searchQuery}
+        onChange={(event) => useDebounce && debouncedSearch(event.target.value)}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            debouncedSearch.cancel();
+            router.push(`/search?q=${event.currentTarget.value}`);
+          }
+        }}
+      />
+    </div>
   );
 }
