ef521c3e8f62f20b2faf7da7223b4b702abd667b
David Mo <44306450+unknown@users.noreply.github.com>
2023-12-30 21:26:06 -0500

    Update auth route

--- apps/web/src/app/api/auth/[...nextauth]/route.ts
+++ apps/web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,3 @@
-import NextAuth from "next-auth";
+import { handlers } from "@/utils/auth";
 
-import { authOptions } from "@/utils/auth";
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+export const { GET, POST } = handlers;
