d750748b9e272c7de94822c8e1049dccaa8cd242
David Mo <44306450+unknown@users.noreply.github.com>
2023-12-31 21:52:59 -0500

    Add unique constraint to store handle

--- packages/db/src/schema/stores.ts
+++ packages/db/src/schema/stores.ts
@@ -1,15 +1,21 @@
 import { relations } from "drizzle-orm";
-import { mysqlTable, serial, text, varchar } from "drizzle-orm/mysql-core";
+import { mysqlTable, serial, text, unique, varchar } from "drizzle-orm/mysql-core";
 
 import { products } from "./products";
 
-export const stores = mysqlTable("Store", {
-  id: serial("id").primaryKey(),
-  handle: varchar("handle", { length: 255 }).notNull(),
-  name: varchar("name", { length: 255 }).notNull(),
-  shortenedName: varchar("shortenedName", { length: 255 }).notNull(),
-  externalUrl: text("externalUrl").notNull(),
-});
+export const stores = mysqlTable(
+  "Store",
+  {
+    id: serial("id").primaryKey(),
+    handle: varchar("handle", { length: 255 }).notNull(),
+    name: varchar("name", { length: 255 }).notNull(),
+    shortenedName: varchar("shortenedName", { length: 255 }).notNull(),
+    externalUrl: text("externalUrl").notNull(),
+  },
+  (store) => ({
+    handleUnq: unique("handleUnq").on(store.handle),
+  }),
+);
 
 export const storesRelations = relations(stores, ({ many }) => {
   return { products: many(products, { relationName: "ProductToStore" }) };
