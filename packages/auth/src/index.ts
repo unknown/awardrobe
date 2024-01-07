26f9f8499da0949ef95f58982ed2cb3c05cc8236
David Mo <44306450+unknown@users.noreply.github.com>
2024-01-07 12:17:42 -0500

    Create auth package

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,45 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import NextAuth from "next-auth";
+import type { DefaultSession, NextAuthConfig } from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+
+import { db } from "@awardrobe/db";
+import { resend, SignInEmail } from "@awardrobe/emails";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+export const config = {
+  adapter: DrizzleAdapter(db),
+  pages: { signIn: "/login" },
+  providers: [
+    EmailProvider({
+      sendVerificationRequest: async ({ identifier, url }) => {
+        await resend.emails.send({
+          to: [identifier],
+          from: "Awardrobe <notifications@getawardrobe.com>",
+          subject: "Sign in to Awardrobe",
+          react: SignInEmail({ url }),
+        });
+      },
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+        },
+      };
+    },
+  },
+} satisfies NextAuthConfig;
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
