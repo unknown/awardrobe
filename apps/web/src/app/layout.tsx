8ae278f47c13ecde5cfbfa4d532782447fb53ba6
David Mo <44306450+unknown@users.noreply.github.com>
2024-01-07 17:17:42 -0500

    Move tRPC provider to app route group

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,16 +1,11 @@
 import { Inter } from "next/font/google";
+import { Toaster } from "@ui/Toaster";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { ThemeProvider } from "@/components/ThemeProvider";
 
 import "@/styles/globals.css";
-
-import { cache } from "react";
-import { headers } from "next/headers";
-import { Toaster } from "@ui/Toaster";
-
-import { TRPCReactProvider } from "@/trpc/react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -35,15 +30,13 @@ type RootLayoutProps = {
   children: React.ReactNode;
 };
 
-const getHeaders = cache(async () => headers());
-
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" className={inter.variable} suppressHydrationWarning>
       <head />
       <body className="bg-background min-h-screen font-sans antialiased">
         <ThemeProvider attribute="class" enableSystem>
-          <TRPCReactProvider headersPromise={getHeaders()}>{children}</TRPCReactProvider>
+          {children}
           <Toaster />
         </ThemeProvider>
         <Analytics />
