1f617504dec21de68823aad48a9b9438acace0c5
David Mo <44306450+unknown@users.noreply.github.com>
2024-01-08 20:49:54 -0500

    Add `delisted` field to products schema

--- packages/db/src/schema/products.ts
+++ packages/db/src/schema/products.ts
@@ -1,5 +1,6 @@
 import { relations } from "drizzle-orm";
 import {
+  boolean,
   index,
   int,
   mysqlTable,
@@ -21,6 +22,7 @@ export const products = mysqlTable(
     storeId: int("storeId").notNull(),
     productCode: varchar("productCode", { length: 255 }).notNull(),
     name: varchar("name", { length: 255 }).notNull(),
+    delisted: boolean("delisted").notNull().default(false),
   },
   (product) => ({
     publicIdIdx: uniqueIndex("publicIdIdx").on(product.publicId),
