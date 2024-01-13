c0ab15773c24cd2af864ff1c3bfcd84ff5780cb3
David Mo <44306450+unknown@users.noreply.github.com>
2024-01-13 15:11:52 -0500

    Add public id to collections

--- packages/db/src/collection.ts
+++ packages/db/src/collection.ts
@@ -3,6 +3,7 @@ import { and, eq } from "drizzle-orm";
 import { db } from "./db";
 import { collections } from "./schema/collections";
 import { Collection } from "./schema/types";
+import { generatePublicId } from "./utils/public-id";
 
 export type FindOrCreateCollectionOptions = {
   brandId: number;
@@ -28,6 +29,7 @@ export async function findOrCreateCollection(
   const collectionsTable = await db.insert(collections).values({
     externalCollectionId,
     brandId,
+    publicId: generatePublicId(),
   });
 
   const created = await db.query.collections.findFirst({
