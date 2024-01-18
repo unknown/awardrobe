001663d92d8e63c1662189c1ee4dcf21bf20dd63
David Mo <44306450+unknown@users.noreply.github.com>
2024-01-18 15:58:58 -0500

    Make sitemap dynamic

--- apps/web/src/app/api/products/revalidate/route.ts
+++ apps/web/src/app/api/products/revalidate/route.ts
@@ -1,7 +1,5 @@
 import { revalidatePath } from "next/cache";
 
-export const revalidate = 0;
-
 export async function GET() {
   revalidatePath("/(app)/(browse)/search", "page");
   return Response.json({ revalidated: true, now: Date.now() });
--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -1,12 +1,17 @@
 import { findListedProducts } from "@awardrobe/db";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 43200; // 12 hours
+
 export default async function sitemap() {
   const routes = ["", "/home", "/login"].map((route) => ({
     url: `https://www.awardrobe.co${route}`,
     lastModified: new Date(),
   }));
 
-  const products = (await findListedProducts()).map((product) => ({
+  const listedProducts = await findListedProducts();
+
+  const products = listedProducts.map((product) => ({
     url: `https://www.awardrobe.co/product/${product.publicId}`,
     lastModified: new Date(), // TODO: make this the last time the product was updated
   }));
